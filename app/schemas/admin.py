1e51036449680df0fc66ae50554b1cde42939759
Toong Yang Wong <wongty27@gmail.com>
2024-11-19 16:11:43 +0800

    delete models and schemas

--- app/schemas/admin.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, Field, SecretStr
-
-class UserMaster(BaseModel):
-    id: int
-    username: str = Field(max_length=20)
-    password: SecretStr = Field(max_length=30)
